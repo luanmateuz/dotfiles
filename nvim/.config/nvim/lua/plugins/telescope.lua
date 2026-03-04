49aa967e3b14a5314b1e2cae5d3e159cda2f3d79
Luan Mateus <luanmateuzz@gmail.com>
2026-03-04 18:49:40 -0300

    nvim: update telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,37 +1,44 @@
 return {
     'nvim-telescope/telescope.nvim',
-    tag = 'v0.1.9',
+    tag = 'v0.2.1',
     dependencies = {
         'nvim-lua/plenary.nvim',
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+        {
+            'nvim-telescope/telescope-fzf-native.nvim',
+            build = 'make',
+            cond = function()
+                return vim.fn.executable 'make' == 1
+            end,
+        },
     },
 
     config = function()
         local telescope = require 'telescope'
 
         telescope.setup {
-            pickers = {
-                find_files = {
-                    find_command = {
-                        'fd',
-                        '--type',
-                        'f',
-                        '--strip-cwd-prefix',
-                        '--hidden',
-                        '--follow',
-                        '--no-ignore-vcs',
-                        '--exclude=.DS_Store',
-                        '--exclude=.git',
-                        '--exclude=node_modules',
-                        '--exclude=.next',
-                    },
+            defaults = {
+                winblend = 0,
+            },
+            extensions = {
+                fzf = {
+                    fuzzy = true,
+                    override_generic_sorter = true,
+                    override_file_sorter = true,
+                    case_mode = 'smart_case',
                 },
             },
         }
+
+        telescope.load_extension 'fzf'
+
         local builtin = require 'telescope.builtin'
         vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+        vim.keymap.set('n', '<leader>fF', function()
+            builtin.find_files { hidden = true }
+        end, { desc = 'Telescope find files (hidden)' })
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+        vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = 'Telescope diagnostics' })
     end,
 }
